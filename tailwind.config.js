531d3a9f7f22068e659bc04249b1c3bf84f4c299
anna14b2 <anna14b2@stud.kea.dk>
2022-02-03 15:13:19 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#CCCCCD",
+      secondary: "#121827",
+      accent: "#FF66BA",
+      graytone: "#EDEDED",
+    },
+    fontFamily: {
+      poppins: "'Poppins', system-ui, sans-serif",
+    },
+
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
